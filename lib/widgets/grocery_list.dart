8fe40122f083f4356915328f60bd27082d246bc2
Aman Kumar <aman58320@gmail.com>
2026-07-02 14:33:01 +0530

    Added FutureBuild but it didn't work with the current logic

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -15,6 +15,7 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
+  // late Future<List<GroceryItem>> _loadedItems;
   List<GroceryItem> _groceryItems = [];
   var _isLoading = true;
   String? _error;
@@ -22,6 +23,7 @@ class _GroceryListState extends State<GroceryList> {
   @override
   initState() {
     super.initState();
+    // _loadedItems = _loadItems();
     _loadItems();
   }
 
@@ -73,6 +75,43 @@ class _GroceryListState extends State<GroceryList> {
       });
     }
   }
+
+  // Future<List<GroceryItem>> _loadItems() async {
+  //   final url = Uri.https(
+  //     apiKey,
+  //     'shopping-list.json',
+  //   );
+  //   final response = await http.get(url);
+  //   if (response.statusCode >= 400) {
+  //     // setState(() {
+  //     //   _error = 'Failed to fetch data. Please try again later ';
+  //     // });
+  //     throw Exception('Failed to fetch data. Please try again later.');
+  //   }
+  //   if (response.body == 'null') {
+  //     return [];
+  //   }
+  //   final Map<String, dynamic> listData = json.decode(
+  //     response.body,
+  //   );
+  //   final List<GroceryItem> loadedItems = [];
+  //   for (final item in listData.entries) {
+  //     final category = categories.entries
+  //         .firstWhere(
+  //           (catItem) => catItem.value.title == item.value['category'],
+  //         )
+  //         .value;
+  //     loadedItems.add(
+  //       GroceryItem(
+  //         id: item.key,
+  //         name: item.value['name'],
+  //         quantity: item.value['quantity'],
+  //         category: category,
+  //       ),
+  //     );
+  //   }
+  //   return loadedItems;
+  // }
 
   void _addItem() async {
     final newItem = await Navigator.of(context).push<GroceryItem>(
@@ -221,3 +260,56 @@ class _GroceryListState extends State<GroceryList> {
     );
   }
 }
+
+
+
+  // FutureBuilder(
+  //         future: _loadedItems,
+  //         builder: (context, snapshot) {
+  //           if (snapshot.connectionState == ConnectionState.waiting) {
+  //             return const Center(
+  //               child: CircularProgressIndicator(
+  //                 color: Color.fromARGB(255, 147, 229, 250),
+  //                 strokeWidth: 2,
+  //               ),
+  //             );
+  //           }
+
+  //           if (snapshot.hasError) {
+  //             return Center(
+  //               child: Text(snapshot.error.toString()),
+  //             );
+  //           }
+
+  //           if (snapshot.data!.isEmpty) {
+  //             return const Center(
+  //               child: Text('No Grocery item added yet.'),
+  //             );
+  //           }
+
+  //           return ListView.builder(
+  //             itemCount: snapshot.data!.length,
+  //             itemBuilder: (context, index) {
+  //               final item = snapshot.data![index];
+  //               return Dismissible(
+  //                 background: Container(
+  //                   margin: const EdgeInsets.symmetric(horizontal: 16),
+  //                   color: Colors.red,
+  //                 ),
+  //                 key: ValueKey(snapshot.data![index].id),
+  //                 onDismissed: (direction) =>
+  //                     _onRemoveGrocery(_groceryItems[index]),
+  //                 child: ListTile(
+  //                   title: Text(item.name.toString()),
+  //                   leading: Container(
+  //                     width: 24,
+  //                     height: 24,
+  //                     color: item.category.color,
+  //                   ),
+  //                   trailing: Text(item.quantity.toString()),
+  //                 ),
+  //               );
+  //             },
+  //           );
+  //         },
+  //       ),
